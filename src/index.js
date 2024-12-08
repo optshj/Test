14abbdab477b73440af863b957c4b60892914c61
optshj <optshj@gmail.com>
2024-12-08 17:29:51 +0900

    sssss

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
-res.send("<h1>돼 라얍</h1>");
+res.send("<h1>돼 라 얍</h1>");
 });
 app.listen(port, () => {
 console.log(`Example app listening at http://localhost:${port}`);
